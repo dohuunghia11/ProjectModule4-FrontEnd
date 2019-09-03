d1404065146e873ce73eb2ac735b945e05870529
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-03 13:53:20 +0700

    create interface

---
+++ src/model/apartment.ts
@@ -0,0 +1,11 @@
+export interface Apartment {
+  id: number;
+  name: string;
+  houseKind: string;
+  roomKind: string;
+  address: string;
+  roomNumber: string;
+  bathNumber: string;
+  description: string;
+  prizePerNight: string;
+}
